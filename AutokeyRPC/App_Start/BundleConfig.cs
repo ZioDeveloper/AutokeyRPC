aaa3d0bfda91e822bad1dfbd238b1e2910d910be
Maurizio Vigna <maurizio.vigna@gmail.com>
2019-05-24 18:00:11 +0200

    24/05 final

--- AutokeyRPC/App_Start/BundleConfig.cs
+++ AutokeyRPC/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace AutokeyRPC
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/mygrid.css"));
         }
     }
 }
